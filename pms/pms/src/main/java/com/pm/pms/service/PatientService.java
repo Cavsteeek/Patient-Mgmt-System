de7a5e413fdb77d280d062bd18ee9c76c9a9d1bc
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-05-27 00:39:11 +0100

    Created patientnotfound exception class to handle those exceptions, created function to update a users details

--- pms/pms/src/main/java/com/pm/pms/service/PatientService.java
+++ pms/pms/src/main/java/com/pm/pms/service/PatientService.java
@@ -4,9 +4,14 @@ import com.pm.pms.dto.PatientRequestDTO;
 import com.pm.pms.dto.PatientResponseDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface PatientService {
     List<PatientResponseDTO> getPatients();
 
     PatientResponseDTO createPatient (PatientRequestDTO patientRequestDTO);
+
+    PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO);
+
+    PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO);
 }
