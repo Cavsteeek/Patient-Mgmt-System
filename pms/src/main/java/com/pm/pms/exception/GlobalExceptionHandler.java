ed60e77a408768996f06f5c6cdc7c2ebdd917aec
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-06-04 14:06:05 +0100

    setup docker environment

---
+++ pms/src/main/java/com/pm/pms/exception/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.pm.pms.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Useful for forms with multiple fields (e.g. registration), where you want to return all errors at once.
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    // Useful when you just want to show one clear error at a time (like for frontend popups or alerts).
+/*
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+    FieldError fieldError = ex.getBindingResult().getFieldError();
+    if (fieldError != null) {
+        return ResponseEntity.badRequest().body(fieldError.getDefaultMessage());
+    }
+    return ResponseEntity.badRequest().body("Validation error");
+}
+
+*/
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<Map<String, String>> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+
+        log.warn("Email address already exist {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Email address already exists");
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(PatientNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePatientNotFoundException(PatientNotFoundException ex) {
+        log.warn("Patient not found {}", ex.getMessage());
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", "Patient not found");
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
+
+
+
+
