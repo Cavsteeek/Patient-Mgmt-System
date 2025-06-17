a16c5fa99048e58c9b98d017f20e190cee9bcf0d
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-06-17 07:57:46 +0100

    commit

---
+++ patient-service/src/main/java/com/pm/pms/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,29 @@
+package com.pm.pms.grpc;
+
+import billing.BillingServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BillingServiceGrpcClient {
+    private static final Logger log = LoggerFactory.getLogger(BillingServiceGrpcClient.class);
+    private final BillingServiceGrpc.BillingServiceBlockingStub blockingStub;
+
+    public BillingServiceGrpcClient(
+            @Value("${billing.service.address:localhost}") String serverAddress,
+            @Value("${billing.service.grpc.port.9001}") int serverPort
+    ) {
+        log.info("Connecting to Billing Service GRPC service at {}:{}", serverAddress, serverAddress);
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress, serverPort).usePlaintext().build();
+
+        blockingStub = BillingServiceGrpc.newBlockingStub(channel);
+    }
+
+}
